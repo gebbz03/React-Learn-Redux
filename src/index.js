dc723344c9754751d457a794d2ce080fbcda3962
unknown <gebb.freelancer@gmail.com>
2020-03-03 09:24:24 +0800

    second-commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore } from "redux";
+import allReducer from "./reducers/";
+// reducers automatically find the index.js
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const myStore = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+//---------------LONG METHOD ---------------
+// //STORE -> GLOBALIZED STATE
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// //ACTION INCREMENT
+// const increment = () => {
+//   return {
+//     type: "INCREMENT"
+//   };
+// };
+
+// const decrement = () => {
+//   return {
+//     type: "DECREMENT"
+//   };
+// };
+
+// //REDUCER
+// const counter = (state = 0, action) => {
+//   switch (action.type) {
+//     case "INCREMENT":
+//       return state + 1;
+//     case "DECREMENT":
+//       return -1;
+//   }
+// };
+
+// let store = createStore(counter);
+// store.subscribe(() => console.log(store.getState()));
+
+// //DISPATCH
+// store.dispatch(increment());
+// store.dispatch(decrement());
+
+ReactDOM.render(
+  <Provider store={myStore}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+
 serviceWorker.unregister();
